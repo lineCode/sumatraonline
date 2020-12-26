8d79d92ef62143e8dc628981273ebe7ad00b1f46
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-12-25 19:21:39 -0800

    add tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script>
   import { fileToShow } from "./store.js";
+  import FS from "https://cdn.skypack.dev/@isomorphic-git/lightning-fs";
 
-  let dragAreaClass = "";
+  let isDraggedOver = false;
 
+  const fs = new FS("files");
+
+  /**
+   * @param {Event} e
+   */
   function preventDefaults(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -10,14 +16,17 @@
 
   function handleDragEnter() {
     console.log("dragEnter");
-    dragAreaClass = "hili";
+    isDraggedOver = true;
   }
 
   function handleDragLeave() {
     console.log("dragLeave");
-    dragAreaClass = "";
+    isDraggedOver = false;
   }
 
+  /**
+   * @param {DragEvent} e
+   */
   function handleDragDrop(e) {
     console.log("dragDrop");
     //preventDefaults(e);
@@ -27,24 +36,16 @@
     const firstFile = files[0];
     console.log("firstFile:", firstFile);
     fileToShow.set(files[0]);
-    window.goToURL("/viewdroppedfile")
+    window.goToURL("/viewdroppedfile");
   }
 </script>
 
 <style>
   #dragarea {
-    width: 80%;
-    margin: 0 auto;
-
     height: 80%;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
 
     border: 1px dashed black;
     background-color: #f2f2f2;
-    font-size: 16pt;
   }
   #dragarea.hili {
     border: 2px dashed blue;
@@ -55,10 +56,11 @@
 <!-- ondragover is vital in Chrome or else default handler will take over -->
 <div
   id="dragarea"
+  class="flex items-center justify-center my-0 mx-auto w-11/12 text-xl"
+  class:hili={isDraggedOver}
   on:dragenter={handleDragEnter}
   on:dragleave={handleDragLeave}
   on:drop|preventDefault|stopPropagation={handleDragDrop}
-  ondragover="return false"
-  class={dragAreaClass}>
+  ondragover="return false">
   <div>Drop PDF file to view.</div>
 </div>
